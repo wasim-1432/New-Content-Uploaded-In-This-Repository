a6023a066239735510a3ca693c0af9e08a5feec7
wasim-1432 <wasim.ahmad0661@gmail.com>
2024-12-28 12:48:54 +0530

    WAP to find the largest number of a given number stored in array.(size of array is 10).

---
+++ OneDrive/Desktop/Array/wasim294.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int arr[10],i,temp;
+    printf("Enter any 10 numbers\n");
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<10;i++)
+    {
+        if(arr[i]>=arr[i+1])
+        {
+            temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+    }
+    for(i=0;i<10;i++)
+    {
+        if(i==9)
+        {
+            printf("Largest Number=%d",arr[i]);
+        }
+    }
+    printf("\n");
+    return 0;
+}
