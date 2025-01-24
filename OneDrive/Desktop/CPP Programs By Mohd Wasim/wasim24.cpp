8c305af5cbabd9604da6207820400ed729dddd3b
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-01-24 10:01:47 +0530

    WAP to print sum of two numbers.

---
+++ OneDrive/Desktop/CPP Programs By Mohd Wasim/wasim24.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter any two numbers"<<endl;
+    cin>>a>>b;
+    cout<<"Addition of two numbers="<<a+b;
+    cout<<endl;
+    return 0;
+}
