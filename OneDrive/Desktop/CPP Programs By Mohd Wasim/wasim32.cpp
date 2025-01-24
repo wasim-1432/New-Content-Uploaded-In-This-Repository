9136e1ba06d0139e720ddd00a47a034189ad1abc
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-01-24 10:25:59 +0530

    WAP to calculate Square of a given number.

---
+++ OneDrive/Desktop/CPP Programs By Mohd Wasim/wasim32.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter any number"<<endl;
+    cin>>a;
+    cout<<"Square of a given number="<<a*a;
+    cout<<endl;
+    return 0;
+}
