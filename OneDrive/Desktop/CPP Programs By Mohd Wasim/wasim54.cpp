705e291b0693d3d599920cc7150bcb0082400802
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-01-27 12:39:17 +0530

    WAP to find the maximum number of given two integer or real numbers usin Function Overloading.

---
+++ OneDrive/Desktop/CPP Programs By Mohd Wasim/wasim54.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int Maximum_Number(int a,int b);
+float Maximum_Number(float a,float b);
+int main()
+{
+    int a,b;
+    cout<<"Enter any two numbers(For Integer Numbers)"<<endl;
+    cin>>a>>b;
+    Maximum_Number(a,b);
+    float x,y;
+    cout<<"Enter any two numbers(For Real numbers)"<<endl;
+    cin>>x>>y;
+    Maximum_Number(x,y);
+    cout<<endl;
+    return 0;
+}
+int Maximum_Number(int a,int b)
+{
+    if(a>b)
+    {
+        cout<<"Maximum number is="<<a<<endl;
+    }
+    else
+    {
+        cout<<"Maximum number is="<<b<<endl;
+    }
+}
+float Maximum_Number(float a,float b)
+{
+    if(a>b)
+    {
+        cout<<"Maximum number is="<<a<<endl;
+    }
+    else
+    {
+        cout<<"Maximum number is="<<b<<endl;
+    }
+}
