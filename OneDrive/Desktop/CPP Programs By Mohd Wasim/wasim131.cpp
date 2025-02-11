9d2d9d85b177ee9bf563dafc92b909ca9daa62f1
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-02-11 06:53:39 +0530

    Define a C++ class fraction: class fraction {      long numerator;      long denominator;      public:           fraction(long n=0,long d=0); }

---
+++ OneDrive/Desktop/CPP Programs By Mohd Wasim/wasim131.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class fraction
+{
+    long numerator;
+    long denominator;
+    public:
+        fraction(long n=0,long d=0){numerator=n;denominator=d;}
+        void showData()
+        {
+            cout<<numerator<<"/"<<denominator<<endl;
+        }
+        fraction operator+(fraction F)
+        {
+            fraction temp;
+            temp.numerator=numerator*F.denominator+F.numerator*denominator;
+            temp.denominator=denominator*F.denominator;
+            temp.Normalise();
+            return temp;
+        }
+        void Normalise()
+        {
+            int i=2;
+            while(i<=numerator && i<=denominator)
+            {
+                if(numerator%i==0 && denominator%i==0)
+                {
+                    numerator=numerator/i;
+                    denominator=denominator/i;
+                    continue;
+                }
+                i++;
+            }
+        }
+};
+int main()
+{
+    fraction f1(2,3),f2(-4,5),f3;
+    f3=f1+f2;
+    f3.showData();
+    cout<<endl;
+    return 0;
+}
